1fe6bc12dbe4ac1071a56c14f3d43cfea232d7a0
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-12-20 17:52:05 +0100

    created unit test for quality decrease by one

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,24 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('Not Aged Brie, Backstage passes to a TAFKAL80ETC concert,' +
+    'Sulfuras, Hand of Ragnaros should decrease quality by one', () => {
+    const gildedRose = new GildedRose(
+      [
+              new Item("+5 Dexterity Vest", 10, 20),
+              new Item("Elixir of the Mongoose", 5, 7)
+      ]
+    );
+    const gildedRoseExpected = new GildedRose(
+      [
+        new Item("+5 Dexterity Vest", 10, 19),
+        new Item("Elixir of the Mongoose", 5, 6)
+      ]
+    );
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    const expectedItems = gildedRoseExpected.items;
+    for (let i=0; i < items.length; i++) {
+      expect(items[i].quality).toBe(expectedItems[i].quality);
+    }
   });
 });
